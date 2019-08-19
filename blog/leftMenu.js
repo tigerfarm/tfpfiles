a8611a38c6032894e6a6fe2c5ac8c10490bf39c8
tigerfarm <tigerfarm@gmail.com>
2019-08-19 13:05:41 -0700

    init new

---
+++ blog/leftMenu.js
@@ -0,0 +1,7 @@
+// Complete left menu items
+document.write('<ul class="navlist">');
+document.write('<li class="leftMenuItem"><a href="../test/index.html">Test page</a></li>');
+// document.write('<li class="leftMenuItem"><a href="../books/PoetryKeats.html">Poetry: Keats</a></li>');
+document.write('<li class="leftMenuItem"><a href="../books/PoetryByron.html">Poetry: Byron</a></li>');
+document.write('</ul>');
+document.write('<hr style="margin-top:9px; width:100%;height:2px; color:gray;" />');
---
+++ books/content/EinsteinCube/content/README.md
@@ -0,0 +1,7 @@
+# Work
+
++ Convert a PDF into a MP4:
+https://video.online-convert.com/convert-to-mp4
+http://www.convertfiles.com/converter.php
+
+Cheers...
